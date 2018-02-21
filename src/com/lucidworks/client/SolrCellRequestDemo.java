a03b76cbf38637a1f54afe84d7a7c12782e2b364
shadowridgedev <shadowridgedev@users.noreply.github.com>
2018-02-21 06:31:06 -0500

    added request demo

---
+++ src/com/lucidworks/client/SolrCellRequestDemo.java
@@ -0,0 +1,25 @@
+package com.lucidworks.client;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.request.ContentStreamUpdateRequest;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.handler.extraction.ExtractingParams;
+
+public class SolrCellRequestDemo {
+	public NamedList<Object> test(String host, String collection, String filepath, String filetype)
+			throws IOException, SolrServerException {
+		SolrClient client = new HttpSolrClient.Builder("http://" + host + ":8983/solr/" + collection).build();
+		ContentStreamUpdateRequest req = new ContentStreamUpdateRequest("/update/extract");
+		req.addFile(new File(filepath), filetype);
+		req.setParam(ExtractingParams.EXTRACT_ONLY, "true");
+		NamedList<Object> result = client.request(req);
+		System.out.println("Result: " + result);
+		return result;
+
+	}
+}
--- src/gutenberg/Gutenberg.java
+++ src/gutenberg/Gutenberg.java
@@ -1,5 +1,8 @@
 package gutenberg;
 
+import org.apache.solr.common.util.NamedList;
+
+import com.lucidworks.client.SolrCellRequestDemo;
 import com.lucidworks.client.SolrInputDocumentWriter;
 import com.lucidworks.client.TestFusionPipelineClient;
 
@@ -9,18 +12,28 @@ public class Gutenberg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		NamedList<Object> result;
+		
 		try {
 			String base = args[0];
 			GuttenbergHelper helper = new GuttenbergHelper(base);
 			FindGuttenbergInfo info = new FindGuttenbergInfo();
 			String sentStr = "Michael McGinn is the mayor of Seattle.";
+		    String filetype = "txt";
+			String filepath = "Z:\\gut\\gutenberg\\0\\1\\1.txt";
+			SolrCellRequestDemo  request = new SolrCellRequestDemo();
+			
+			
+			result = request.test("610-2", "Gutenberg-Default", filepath, filetype);
+			System.out.println("Result: " + result);
+			
+			
 			Test = new ReverbTest();
 
 			TestFusionPipelineClient connectTest = new TestFusionPipelineClient();
 			
-//			Openiework ie = new Openiework();
-//			ie.test();
+			Openiework ie = new Openiework();
+			Openiework.test();
 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
