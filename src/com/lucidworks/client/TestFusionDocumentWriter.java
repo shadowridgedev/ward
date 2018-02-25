2739343ffdea3a252eb305cedc57ccd36a100063
shadowridgedev <shadowridgedev@users.noreply.github.com>
2018-02-24 23:39:30 -0500

    added fusion properties

--- src/com/lucidworks/client/TestFusionDocumentWriter.java
+++ src/com/lucidworks/client/TestFusionDocumentWriter.java
@@ -61,7 +61,7 @@ public class TestFusionDocumentWriter {
 
   static {
 
-    try (InputStream in = new FileInputStream("properties/properties.xml")) {
+    try (InputStream in = new FileInputStream("properties/propertiesfusion.xml")) {
       Properties prop = new Properties();
       prop.loadFromXML(in);
 
