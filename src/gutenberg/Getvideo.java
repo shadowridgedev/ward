270fbb2f46af738192fdb00b70a616a64670dd0c
shadowridgedev <shadowridgedev@users.noreply.github.com>
2018-03-06 12:50:06 -0500

    Renamed class

--- src/gutenberg/Getvideo.java
+++ src/gutenberg/Getvideo.java
@@ -9,9 +9,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 
-public class getvideo {
+public class Getvideo {
 
-getvideo ()  {
+Getvideo ()  {
 	
    
  
