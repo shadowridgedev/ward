04761ff75cec4ef60f345abfd57c33bef0d29caa
shadowridgedev <shadowridgedev@users.noreply.github.com>
2019-12-24 23:01:13 -0500

    app

--- src/net/myexperiments/ward/jsoup/App.java
+++ src/net/myexperiments/ward/jsoup/App.java
@@ -4,12 +4,10 @@ package net.myexperiments.ward.jsoup;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-        FirstJsoupExample souptest =  new FirstJsoupExample();
-        String url = "https://youtu.be/NNa5g_d_IJA";
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		FirstJsoupExample souptest = new FirstJsoupExample();
+		String url = "https://youtu.be/NNa5g_d_IJA";
+	}
 }
