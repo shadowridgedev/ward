a4be279ce132d8ed0231a5352ae99f424ccc92a3
work <work@0.0.16.6>
2020-01-16 09:25:19 -0500

    update

--- src/com/myexperiments/ward/Gutenberg.java
+++ src/com/myexperiments/ward/Gutenberg.java
@@ -1,82 +1,51 @@
 package com.myexperiments.ward;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
-import org.apache.uima.UIMAException;
-import org.apache.uima.analysis_engine.AnalysisEngine;
-import org.apache.uima.analysis_engine.AnalysisEngineDescription;
-import org.apache.uima.collection.CollectionReader;
-import org.apache.uima.fit.factory.AggregateBuilder;
-import org.apache.uima.fit.factory.AnalysisEngineFactory;
-import org.apache.uima.fit.pipeline.SimplePipeline;
-import org.apache.uima.fit.testing.util.HideOutput;
-import org.apache.uima.resource.ResourceInitializationException;
-
-import org.apache.uima.UimaContext;
-import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.fit.descriptor.TypeCapability;
-import org.apache.uima.jcas.JCas;
-import org.cleartk.ml.CleartkSequenceAnnotator;
-import org.cleartk.ml.Feature;
-import org.cleartk.ml.Instances;
-import org.cleartk.ml.chunking.BioChunking;
-import org.cleartk.ml.feature.extractor.CleartkExtractor;
-import org.cleartk.ml.feature.extractor.CleartkExtractor.Following;
-import org.cleartk.ml.feature.extractor.CleartkExtractor.Preceding;
-import org.cleartk.ml.feature.extractor.CombinedExtractor1;
-import org.cleartk.ml.feature.extractor.CoveredTextExtractor;
-import org.cleartk.ml.feature.extractor.FeatureExtractor1;
-import org.cleartk.ml.feature.function.CharacterCategoryPatternFunction;
-import org.cleartk.ml.feature.function.FeatureFunctionExtractor;
-import org.cleartk.ml.jar.DefaultSequenceDataWriterFactory;
-import org.cleartk.ml.jar.DirectoryDataWriterFactory;
-import org.cleartk.ml.jar.GenericJarClassifierFactory;
 import org.mapdb.DBMaker;
 import org.mapdb.DBMaker.Maker;
 
-import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
-import static org.apache.uima.fit.pipeline.SimplePipeline.runPipeline;
-import static org.apache.uima.fit.factory.AnalysisEngineFactory.*;
-
-import edu.stanford.nlp.pipeline.SentenceAnnotator;
-import opennlp.tools.stemmer.snowball.SnowballStemmer;
-// test
-
-/*
-import org.ie4opendata.octroy.ContractClassifier;
-import org.ie4opendata.octroy.AmountAnnotator;
-import org.ie4opendata.octroy.FilesCollectionReader;
-import org.ie4opendata.octroy.ReasonAnnotator;
-import org.ie4opendata.octroy.ContractFlowController;
-import org.ie4opendata.octroy.TsvWriter;
-*/
 public class Gutenberg {
 	static ArrayList<String> options;
 	static String PropPath;
 	static Prop prop;
+	static Properties Theprop;
 
 	static public void main(String[] args) throws Exception {
-//		URL url = new URL("https://www.w3.org/TR/PNG/iso_8859-1.txt");
-		URL url = new URL("https://youtu.be/4scs6J33B9E");
-		File f = new File("H:/test.txt");
-		Getvideo video = new Getvideo(url, f);
 		/*
-		 * options = getargs(args); String PropPath = System.getProperty("user.dir") +
-		 * "\\" + options.get(0); Prop prop = new Prop(PropPath); if (options != null) {
-		 * 
-		 * String function = options.get(1);
-		 * 
-		 * if (function.equals("youtube")) { URL url = new
+		 * URL url = new URL("https://www.w3.org/TR/PNG/iso_8859-1.txt"); URL url = new
 		 * URL("https://youtu.be/4scs6J33B9E"); File f = new File("H:/test.txt");
-		 * Getvideo video = new Getvideo(url, f); } if (function.equals("GetFiles")) {
-		 * doparse(); } }
+		 * Getvideo video = new Getvideo(url, f);
 		 */
+
+		options = getargs(args);
+		String PropPath = System.getProperty("user.dir") + "/properties";
+
+		PropPath += "/" + options.get(3);
+
+		Properties prop = new Prop(PropPath).theProp;
+
+		if (options != null) {
+
+			String function = options.get(1);
+
+			if (function.equals("youtube")) {
+				URL url = new URL("https://youtu.be/4scs6J33B9E");
+				File f = new File(PropPath + "videotest.txt");
+				Getvideo video = new Getvideo(url, f);
+
+			}
+			if (function.equals("GetFiles")) {
+				doparse();
+			}
+		}
+
 	}
 
 	static public ArrayList<String> getargs(String[] args) {
