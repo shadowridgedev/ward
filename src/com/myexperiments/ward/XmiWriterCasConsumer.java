570d0b312061729bc8500a9add366668bd88d058
shadowridgedev <shadowridgedev@users.noreply.github.com>
2020-03-09 21:53:49 -0400

    download youtube work

--- src/com/myexperiments/ward/XmiWriterCasConsumer.java
+++ src/com/myexperiments/ward/XmiWriterCasConsumer.java
@@ -92,7 +92,7 @@ public class XmiWriterCasConsumer extends CasConsumer_ImplBase {
 			throw new ResourceProcessException(e);
 		}
 		// retrieve the filename of the input file from the CAS
-		FSIterator it = jcas.getAnnotationIndex(SourceDocumentInformation.type).iterator();
+		FSIterator<?> it = jcas.getAnnotationIndex(SourceDocumentInformation.type).iterator();
 		File outFile = null;
 		if (it.hasNext()) {
 			SourceDocumentInformation fileLoc = (SourceDocumentInformation) it.next();
