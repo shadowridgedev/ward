5f3bab26d72b4c80f858e22f92c10b789e5398de
work <work@0.0.16.6>
2020-03-12 01:12:25 -0400

    Book test

--- src/com/myexperiments/ward/Book.java
+++ src/com/myexperiments/ward/Book.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+// change
 @Entity
 @Table(name = "book")
 public class Book implements Serializable {
