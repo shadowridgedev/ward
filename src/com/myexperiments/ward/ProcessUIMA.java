80044b72527ec404d21d66e25907414d9d8c061d
shadowridgedev <shadowridgedev@users.noreply.github.com>
2021-03-07 20:08:09 -0500

    update

---
+++ src/com/myexperiments/ward/ProcessUIMA.java
@@ -0,0 +1,19 @@
+package com.myexperiments.ward;
+
+import java.io.File;
+
+public class ProcessUIMA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		File taeDescriptor = null;
+		File inputDir = null;
+		ExampleApplication exampleApplication = null;
+
+	}
+
+	void Processonefile() {
+
+	}
+
+}
